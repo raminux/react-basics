99e492b19a48f67ed5d194d4b515ce9eae242771
raminux <ramin.esmzad@hotmail.com>
2022-04-09 19:18:44 +0430

    Component reuseability example using map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
-function Food({ type }) {
-  return <h2>I loke {type}</h2>
+function Food({ name, picture }) {
+  return <div>
+    <h1>I like {name}</h1>
+    <img src={picture} />
+  </div>;
 }
+
+const foodILike = [
+  { name: 'Salad', picture: 'https://wp-cdn.lingokids.com/wp-content/uploads/2020/11/20182F012F232F082F532F372F634819c1-ce43-458a-8682-0ecabb2bfd0f2FSalad.png' },
+  { name: 'Sandwich', picture: 'https://wp-cdn.lingokids.com/wp-content/uploads/2020/11/20182F012F232F082F532F152F57431742-cd03-4030-86b0-6912ed51b4952FSandwich.png' },
+  { name: 'Grape juice', picture: 'https://wp-cdn.lingokids.com/wp-content/uploads/2020/11/20172F112F302F152F442F132F9c6d8774-1e77-44c4-a16f-ac59c149ac0d2FGrapeJuice.png' }
+]
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
-      <Food type="Soup" />
-      <Food type="Kimchi" />
-      <Food type="Ramen" />
+      {foodILike.map((dish) => <Food name={dish.name} picture={dish.picture} />)}
     </div>
   );
 }
