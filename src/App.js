507ab72f81b47c0aaccfe177019051a9847f72be
raminux <ramin.esmzad@hotmail.com>
2022-04-09 20:40:56 +0430

    React typechecking using prop-types

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({ name, picture }) {
+
+const foodILike = [
+  { id: 1, name: 'Salad', rating: 4.3, picture: 'https://wp-cdn.lingokids.com/wp-content/uploads/2020/11/20182F012F232F082F532F372F634819c1-ce43-458a-8682-0ecabb2bfd0f2FSalad.png' },
+  { id: 2, name: 'Sandwich', rating: 1.4, picture: 'https://wp-cdn.lingokids.com/wp-content/uploads/2020/11/20182F012F232F082F532F152F57431742-cd03-4030-86b0-6912ed51b4952FSandwich.png' },
+  { id: 3, name: 'Grape juice', rating: 2.3, picture: 'https://wp-cdn.lingokids.com/wp-content/uploads/2020/11/20172F112F302F152F442F132F9c6d8774-1e77-44c4-a16f-ac59c149ac0d2FGrapeJuice.png' }
+]
+
+function Food({ name, picture, rating }) {
   return <div>
     <h1>I like {name}</h1>
-    <img src={picture} />
+    <h4>{rating}/5.0</h4>
+    <img src={picture} alt={name} />
   </div>;
 }
 
-const foodILike = [
-  { id: 1, name: 'Salad', picture: 'https://wp-cdn.lingokids.com/wp-content/uploads/2020/11/20182F012F232F082F532F372F634819c1-ce43-458a-8682-0ecabb2bfd0f2FSalad.png' },
-  { id: 2, name: 'Sandwich', picture: 'https://wp-cdn.lingokids.com/wp-content/uploads/2020/11/20182F012F232F082F532F152F57431742-cd03-4030-86b0-6912ed51b4952FSandwich.png' },
-  { id: 3, name: 'Grape juice', picture: 'https://wp-cdn.lingokids.com/wp-content/uploads/2020/11/20172F112F302F152F442F132F9c6d8774-1e77-44c4-a16f-ac59c149ac0d2FGrapeJuice.png' }
-]
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  picture: PropTypes.string.isRequired,
+}
+
 
 function App() {
   return (
     <div className="App">
-      {foodILike.map((dish) => <Food key={dish.id} name={dish.name} picture={dish.picture} />)}
+      {foodILike.map((dish) => <Food key={dish.id} name={dish.name} picture={dish.picture} rating={dish.rating} />)}
     </div>
   );
 }
