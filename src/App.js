0886cf0140e505a8eb803a09b3ebb09a4ee47258
raminux <ramin.esmzad@hotmail.com>
2022-04-15 07:53:14 -0700

    React Movie app - render Movie component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
-
   state = {
     isLoading: true,
     movies: [],
   };
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-  }
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+    console.log(movies);
+  };
 
   componentDidMount() {
     this.getMovies();
   }
 
-
   render() {
+    const { isLoading, movies } = this.state;
 
-    const { isLoading } = this.state;
-
-    return <div>
-      {isLoading ? "Loading..." : "We are ready"}
-    </div>;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+      </div>
+    );
   }
 }
 export default App;
